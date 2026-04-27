2ac36ee4a9de278de02700f2757e9cdbf6055f34
Mark-El <phellearnex@gmail.com>
2026-04-27 20:19:42 +0000

    fix: changed firebase project base

--- checkedin/firebase.js
+++ checkedin/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxIdGmZOVAV-Cu7fuu4ozMvO6pLYYdWeY",
-  authDomain: "checkedin-3a722.firebaseapp.com",
-  projectId: "checkedin-3a722",
-  storageBucket: "checkedin-3a722.firebasestorage.app",
-  messagingSenderId: "965132902920",
-  appId: "1:965132902920:web:73a43516b595eac45de7bd",
+  apiKey: "AIzaSyD3C6fQ6q6ZVL_SdOeGefjv7LsYx3OMJDw",
+  authDomain: "checkedin-app-e5688.firebaseapp.com",
+  projectId: "checkedin-app-e5688",
+  storageBucket: "checkedin-app-e5688.firebasestorage.app",
+  messagingSenderId: "133359152998",
+  appId: "1:133359152998:web:2b019cf1ef293326fce2a7",
 };
 
 const app = initializeApp(firebaseConfig);
