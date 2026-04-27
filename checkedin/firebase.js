4784b8e3d3eb46e4fff164f6b263eec5aa318188
Mark-El <phellearnex@gmail.com>
2026-04-27 16:31:52 +0000

    config: add firebase setup

---
+++ checkedin/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxIdGmZOVAV-Cu7fuu4ozMvO6pLYYdWeY",
+  authDomain: "checkedin-3a722.firebaseapp.com",
+  projectId: "checkedin-3a722",
+  storageBucket: "checkedin-3a722.firebasestorage.app",
+  messagingSenderId: "965132902920",
+  appId: "1:965132902920:web:73a43516b595eac45de7bd",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
