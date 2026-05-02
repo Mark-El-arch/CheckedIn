e32fd413c192d5db7f17ce303db3db1e288a36b4
Mark-El <phellearnex@gmail.com>
2026-05-02 03:21:27 +0000

    fix: revert to getAuth to fix Android crash on launch

--- checkedin/firebase.js
+++ checkedin/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 const firebaseConfig = {
@@ -15,7 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+export const auth = getAuth(app);
 export const db = getFirestore(app);
